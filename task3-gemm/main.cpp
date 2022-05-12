011397b61fb5836ad45b7e8041fb6c655317ffd2
VichyTong <vichytong@163.com>
2022-05-12 14:30:13 +0800

    Task3:Used openMP, still found SIMD makes my program slower.

--- task3-gemm/main.cpp
+++ task3-gemm/main.cpp
@@ -8,6 +8,7 @@
 #include <cassert>
 #include <emmintrin.h>
 #include <immintrin.h>
+#include <omp.h>
 
 #define PRINT_TIME(code) do { \
     auto start = system_clock::now(); \
@@ -21,21 +22,40 @@ using namespace std;
 
 using namespace chrono;
 
-using vec = vector<int>; 
+using vec = vector<int>;
 
 const int scale[] = {256, 512, 1024, 2048};
 const string data_path("./data/");
 
 int A[2060 * 2060], B[2060 * 2060], C[2060 * 2060];
 
-typedef union{
-    __m128i v;
-    int d[4];
-} v4i;
+#define mc 256
+#define kc 128
 
-void AddDot4x4(const int &size, int *a, int *b, int *c){
+/*
+void PackA(int k, int *a, int lda, int *a_to){
+    int *a_i0_ptr = &a[0], *a_i1_ptr = &a[lda], *a_i2_ptr = &a[lda * 2], *a_i3_ptr = &a[lda * 3];
+    for(int j = 0; j < k; j++){
+        *a_to ++ = *a_i0_ptr ++;
+        *a_to ++ = *a_i1_ptr ++;
+        *a_to ++ = *a_i2_ptr ++;
+        *a_to ++ = *a_i3_ptr ++;
+    }
+}
+void PackB(int k, int *b, int ldb, int *b_to){
+    for(int i = 0; i < k; i++){
+        int *b_ij_ptr = &b[i * ldb];
+        *(b_to + 0) = *b_ij_ptr;
+        *(b_to + 1) = *(b_ij_ptr + 1);
+        *(b_to + 2) = *(b_ij_ptr + 2);
+        *(b_to + 3) = *(b_ij_ptr + 3);
+        b_to += 4;
+    }
+} */
+
+void AddDot4x4(int K, int *a, int lda, int *b, int ldb, int *c, int ldc){
     int k;
-    v4i
+    __m128i
         c_00_c_01_c_02_c_03_reg,
         c_10_c_11_c_12_c_13_reg,
         c_20_c_21_c_22_c_23_reg,
@@ -45,65 +65,57 @@ void AddDot4x4(const int &size, int *a, int *b, int *c){
 
     int *a0p_ptr, *a1p_ptr, *a2p_ptr, *a3p_ptr;
     a0p_ptr = &a[0];
-    a1p_ptr = &a[size];
-    a2p_ptr = &a[size * 2];
-    a3p_ptr = &a[size * 3];
+    a1p_ptr = &a[lda];
+    a2p_ptr = &a[lda * 2];
+    a3p_ptr = &a[lda * 3];
 
-    c_00_c_01_c_02_c_03_reg.v = _mm_setzero_si128();
-    c_10_c_11_c_12_c_13_reg.v = _mm_setzero_si128();
-    c_20_c_21_c_22_c_23_reg.v = _mm_setzero_si128();
-    c_30_c_31_c_32_c_33_reg.v = _mm_setzero_si128();
+    c_00_c_01_c_02_c_03_reg = _mm_setzero_si128();
+    c_10_c_11_c_12_c_13_reg = _mm_setzero_si128();
+    c_20_c_21_c_22_c_23_reg = _mm_setzero_si128();
+    c_30_c_31_c_32_c_33_reg = _mm_setzero_si128();
 
-    for(k = 0; k < size; k++){
-        b_p0_b_p1_b_p2_b_p3_reg.v = _mm_set_epi32(b[k * size + 3], b[k * size + 2], b[k * size + 1], b[k * size]);
-        int ap = *a0p_ptr;
-//        printf("*** %d ***\n",ap);
-        a_0p_reg.v = _mm_set_epi32(ap, ap, ap, ap);
+    for(k = 0; k < K; k++){
+        b_p0_b_p1_b_p2_b_p3_reg = _mm_load_si128((const __m128i*) &b[k * ldb]);
+        register int ap = *a0p_ptr;
+        a_0p_reg = _mm_set_epi32(ap, ap, ap, ap);
         ++ a0p_ptr;
 
         ap = *a1p_ptr;
-//        printf("*** %d ***\n",ap);
-        a_1p_reg.v = _mm_set_epi32(ap, ap, ap, ap);
+        a_1p_reg = _mm_set_epi32(ap, ap, ap, ap);
         ++ a1p_ptr;
 
         ap = *a2p_ptr;
-//        printf("*** %d ***\n",ap);
-        a_2p_reg.v = _mm_set_epi32(ap, ap, ap, ap);
+        a_2p_reg = _mm_set_epi32(ap, ap, ap, ap);
         ++ a2p_ptr;
 
         ap = *a3p_ptr;
-//        printf("*** %d ***\n",ap);
-        a_3p_reg.v = _mm_set_epi32(ap, ap, ap, ap);
+        a_3p_reg = _mm_set_epi32(ap, ap, ap, ap);
         ++ a3p_ptr;
 
-//        getchar();
-        c_00_c_01_c_02_c_03_reg.v = _mm_add_epi32( c_00_c_01_c_02_c_03_reg.v, _mm_mullo_epi32(a_0p_reg.v, b_p0_b_p1_b_p2_b_p3_reg.v));
-        c_10_c_11_c_12_c_13_reg.v = _mm_add_epi32( c_10_c_11_c_12_c_13_reg.v, _mm_mullo_epi32(a_1p_reg.v, b_p0_b_p1_b_p2_b_p3_reg.v));
-        c_20_c_21_c_22_c_23_reg.v = _mm_add_epi32( c_20_c_21_c_22_c_23_reg.v, _mm_mullo_epi32(a_2p_reg.v, b_p0_b_p1_b_p2_b_p3_reg.v));
-        c_30_c_31_c_32_c_33_reg.v = _mm_add_epi32( c_30_c_31_c_32_c_33_reg.v, _mm_mullo_epi32(a_3p_reg.v, b_p0_b_p1_b_p2_b_p3_reg.v));
-//        printf("%d %d %d %d\n",c_00_c_01_c_02_c_03_reg.d[0],c_00_c_01_c_02_c_03_reg.d[1],c_00_c_01_c_02_c_03_reg.d[2],c_00_c_01_c_02_c_03_reg.d[3]);
-//        printf("%d %d %d %d\n",c_10_c_11_c_12_c_13_reg.d[0],c_10_c_11_c_12_c_13_reg.d[1],c_10_c_11_c_12_c_13_reg.d[2],c_10_c_11_c_12_c_13_reg.d[3]);
-//        printf("%d %d %d %d\n",c_20_c_21_c_22_c_23_reg.d[0],c_20_c_21_c_22_c_23_reg.d[1],c_20_c_21_c_22_c_23_reg.d[2],c_20_c_21_c_22_c_23_reg.d[3]);
-//        printf("%d %d %d %d\n",c_30_c_31_c_32_c_33_reg.d[0],c_30_c_31_c_32_c_33_reg.d[1],c_30_c_31_c_32_c_33_reg.d[2],c_30_c_31_c_32_c_33_reg.d[3]);
-//        getchar();
+        c_00_c_01_c_02_c_03_reg = _mm_add_epi32( c_00_c_01_c_02_c_03_reg, _mm_mullo_epi32(a_0p_reg, b_p0_b_p1_b_p2_b_p3_reg));
+        c_10_c_11_c_12_c_13_reg = _mm_add_epi32( c_10_c_11_c_12_c_13_reg, _mm_mullo_epi32(a_1p_reg, b_p0_b_p1_b_p2_b_p3_reg));
+        c_20_c_21_c_22_c_23_reg = _mm_add_epi32( c_20_c_21_c_22_c_23_reg, _mm_mullo_epi32(a_2p_reg, b_p0_b_p1_b_p2_b_p3_reg));
+        c_30_c_31_c_32_c_33_reg = _mm_add_epi32( c_30_c_31_c_32_c_33_reg, _mm_mullo_epi32(a_3p_reg, b_p0_b_p1_b_p2_b_p3_reg));
+
     }
 
-    c[0 * size + 0] += c_00_c_01_c_02_c_03_reg.d[0];
-    c[0 * size + 1] += c_00_c_01_c_02_c_03_reg.d[1];
-    c[0 * size + 2] += c_00_c_01_c_02_c_03_reg.d[2];
-    c[0 * size + 3] += c_00_c_01_c_02_c_03_reg.d[3];
-    c[1 * size + 0] += c_10_c_11_c_12_c_13_reg.d[0];
-    c[1 * size + 1] += c_10_c_11_c_12_c_13_reg.d[1];
-    c[1 * size + 2] += c_10_c_11_c_12_c_13_reg.d[2];
-    c[1 * size + 3] += c_10_c_11_c_12_c_13_reg.d[3];
-    c[2 * size + 0] += c_20_c_21_c_22_c_23_reg.d[0];
-    c[2 * size + 1] += c_20_c_21_c_22_c_23_reg.d[1];
-    c[2 * size + 2] += c_20_c_21_c_22_c_23_reg.d[2];
-    c[2 * size + 3] += c_20_c_21_c_22_c_23_reg.d[3];
-    c[3 * size + 0] += c_30_c_31_c_32_c_33_reg.d[0];
-    c[3 * size + 1] += c_30_c_31_c_32_c_33_reg.d[1];
-    c[3 * size + 2] += c_30_c_31_c_32_c_33_reg.d[2];
-    c[3 * size + 3] += c_30_c_31_c_32_c_33_reg.d[3];
+    __m128i tmp = _mm_load_si128((const __m128i*) &c[0]);
+    _mm_store_si128((__m128i *) &c[0], _mm_add_epi32(c_00_c_01_c_02_c_03_reg, tmp));
+    tmp = _mm_load_si128((const __m128i*) &c[ldc]);
+    _mm_store_si128((__m128i *) &c[ldc], _mm_add_epi32(c_10_c_11_c_12_c_13_reg, tmp));
+    tmp = _mm_load_si128((const __m128i*) &c[2 * ldc]);
+    _mm_store_si128((__m128i *) &c[2 * ldc], _mm_add_epi32(c_20_c_21_c_22_c_23_reg, tmp));
+    tmp = _mm_load_si128((const __m128i*) &c[3 * ldc]);
+    _mm_store_si128((__m128i *) &c[3 * ldc], _mm_add_epi32(c_30_c_31_c_32_c_33_reg, tmp));
+}
+
+void InnerKernel(int n, int m, int k, int *a, int lda, int *b, int ldb, int *c, int ldc){
+#pragma omp parallel for
+    for (int i = 0; i < n ; i += 4 ) {
+        for (int j = 0; j < m; j += 4 ) {
+            AddDot4x4(k, &a[i * lda], lda, &b[j], ldb, &c[i * ldc + j], ldc);
+        }
+    }
 }
 void Gemm(const int &size, vec &a, vec &b, vec &c) {
     for(int i = 0; i < size; i++){
@@ -113,9 +125,13 @@ void Gemm(const int &size, vec &a, vec &b, vec &c) {
             C[i * size + j] = 0;
         }
     }
-    for(int i = 0; i < size; i+=4){
-        for(int j = 0; j < size; j+=4){
-            AddDot4x4(size, &A[i * size], &B[j], &C[i * size + j]);
+    const int N = size;
+    int kb, jb;
+    for (int k = 0; k < size; k += kc ){
+        kb = min(N - k, kc);
+        for (int j = 0; j < size; j += mc ){
+            jb = min(N - j, mc);
+            InnerKernel(N, jb, kb, &A[k], N, &B[k * N + j], N, &C[j], N);
         }
     }
     for(int i = 0; i < size; i++){
@@ -163,7 +179,7 @@ void Benchmark(const int &size) {
     PRINT_TIME(
        Gemm(size, a, b, c);
     );
-    
+
     CheckResult(c, result_path);
 
     file_a.close();
