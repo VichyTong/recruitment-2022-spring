becf0d04a5e689188890c8c2fe6e38da0fa2f22b
VichyTong <vichytong@163.com>
2022-05-12 15:03:39 +0800

    Task3: Tried former no SIMD program, using openMP, found that it's 2x faster. :(

--- task3-gemm/main.cpp
+++ task3-gemm/main.cpp
@@ -32,7 +32,7 @@ int A[2060 * 2060], B[2060 * 2060], C[2060 * 2060];
 #define mc 256
 #define kc 128
 
-/*
+
 void PackA(int k, int *a, int lda, int *a_to){
     int *a_i0_ptr = &a[0], *a_i1_ptr = &a[lda], *a_i2_ptr = &a[lda * 2], *a_i3_ptr = &a[lda * 3];
     for(int j = 0; j < k; j++){
@@ -51,7 +51,7 @@ void PackB(int k, int *b, int ldb, int *b_to){
         *(b_to + 3) = *(b_ij_ptr + 3);
         b_to += 4;
     }
-} */
+}
 
 void AddDot4x4(int K, int *a, int lda, int *b, int ldb, int *c, int ldc){
     int k;
@@ -109,6 +109,64 @@ void AddDot4x4(int K, int *a, int lda, int *b, int ldb, int *c, int ldc){
     _mm_store_si128((__m128i *) &c[3 * ldc], _mm_add_epi32(c_30_c_31_c_32_c_33_reg, tmp));
 }
 
+void AddDot1x4(const int &size, int *a, int *b, int *c){ // no SIMD
+    int k;
+    register int
+            c_00_reg = 0,   c_01_reg = 0,   c_02_reg = 0,   c_03_reg = 0,
+            c_10_reg = 0,   c_11_reg = 0,   c_12_reg = 0,   c_13_reg = 0,
+            c_20_reg = 0,   c_21_reg = 0,   c_22_reg = 0,   c_23_reg = 0,
+            c_30_reg = 0,   c_31_reg = 0,   c_32_reg = 0,   c_33_reg = 0,
+            b_0p_reg = 0,   b_1p_reg = 0,   b_2p_reg = 0,   b_3p_reg = 0,
+            a_p0_reg = 0,   a_p1_reg = 0,   a_p2_reg = 0,   a_p3_reg = 0;
+
+    int *ap0_ptr, *ap1_ptr, *ap2_ptr, *ap3_ptr;
+    ap0_ptr = &a[0];
+    ap1_ptr = &a[size];
+    ap2_ptr = &a[size * 2];
+    ap3_ptr = &a[size * 3];
+
+    for(k = 0; k < size; k++){
+        b_0p_reg = b[k * size];
+        b_1p_reg = b[k * size + 1];
+        b_2p_reg = b[k * size + 2];
+        b_3p_reg = b[k * size + 3];
+
+        a_p0_reg = *ap0_ptr ++;
+        a_p1_reg = *ap1_ptr ++;
+        a_p2_reg = *ap2_ptr ++;
+        a_p3_reg = *ap3_ptr ++;
+
+        c_00_reg += a_p0_reg * b_0p_reg;
+        c_10_reg += a_p1_reg * b_0p_reg;
+
+        c_01_reg += a_p0_reg * b_1p_reg;
+        c_11_reg += a_p1_reg * b_1p_reg;
+
+        c_02_reg += a_p0_reg * b_2p_reg;
+        c_12_reg += a_p1_reg * b_2p_reg;
+
+        c_03_reg += a_p0_reg * b_3p_reg;
+        c_13_reg += a_p1_reg * b_3p_reg;
+
+        c_20_reg += a_p2_reg * b_0p_reg;
+        c_30_reg += a_p3_reg * b_0p_reg;
+
+        c_21_reg += a_p2_reg * b_1p_reg;
+        c_31_reg += a_p3_reg * b_1p_reg;
+
+        c_22_reg += a_p2_reg * b_2p_reg;
+        c_32_reg += a_p3_reg * b_2p_reg;
+
+        c_23_reg += a_p2_reg * b_3p_reg;
+        c_33_reg += a_p3_reg * b_3p_reg;
+    }
+
+    c[0 * size + 0] += c_00_reg; c[0 * size + 1] += c_01_reg; c[0 * size + 2] += c_02_reg; c[0 * size + 3] += c_03_reg;
+    c[1 * size + 0] += c_10_reg; c[1 * size + 1] += c_11_reg; c[1 * size + 2] += c_12_reg; c[1 * size + 3] += c_13_reg;
+    c[2 * size + 0] += c_20_reg; c[2 * size + 1] += c_21_reg; c[2 * size + 2] += c_22_reg; c[2 * size + 3] += c_23_reg;
+    c[3 * size + 0] += c_30_reg; c[3 * size + 1] += c_31_reg; c[3 * size + 2] += c_32_reg; c[3 * size + 3] += c_33_reg;
+}
+
 void InnerKernel(int n, int m, int k, int *a, int lda, int *b, int ldb, int *c, int ldc){
 #pragma omp parallel for
     for (int i = 0; i < n ; i += 4 ) {
@@ -125,15 +183,22 @@ void Gemm(const int &size, vec &a, vec &b, vec &c) {
             C[i * size + j] = 0;
         }
     }
-    const int N = size;
-    int kb, jb;
-    for (int k = 0; k < size; k += kc ){
-        kb = min(N - k, kc);
-        for (int j = 0; j < size; j += mc ){
-            jb = min(N - j, mc);
-            InnerKernel(N, jb, kb, &A[k], N, &B[k * N + j], N, &C[j], N);
+//    const int N = size;
+//    int kb, jb;
+//    for (int k = 0; k < size; k += kc ){
+//        kb = min(N - k, kc);
+//        for (int j = 0; j < size; j += mc ){
+//            jb = min(N - j, mc);
+//            InnerKernel(N, jb, kb, &A[k], N, &B[k * N + j], N, &C[j], N);
+//        }
+//    }
+#pragma omp parallel for
+    for(int i = 0; i < size; i+=4){
+        for(int j = 0; j < size; j+=4){
+            AddDot1x4(size, &A[i * size], &B[j], &C[i * size + j]);
         }
     }
+
     for(int i = 0; i < size; i++){
         for(int j = 0; j < size; j++){
             c[i * size + j] = C[i * size + j];
@@ -145,10 +210,6 @@ void CheckResult(const vec &c, const string &result_path) {
     ifstream file_result(result_path);
     int nelems = c.size();
     float res_i;
-//    for(int i = 0; i < nelems; i++){
-//        printf("%d ", c[i]);
-//    }
-//    getchar();
     for(int i = 0; i < nelems; i++) {
         file_result >> res_i;
         assert(c[i] == res_i);
